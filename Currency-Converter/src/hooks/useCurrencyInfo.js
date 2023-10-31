32bd9e9e1e316d7395a50ee406e2bfb205a2bc90
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2023-10-31 23:03:13 +0530

    Custom hook Setup

--- Currency-Converter/src/hooks/useCurrencyInfo.js
+++ Currency-Converter/src/hooks/useCurrencyInfo.js
@@ -1,14 +1,14 @@
-import {  useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 
 function useCurrency(currency) {
-    const [ data , setData] = useState({})
+    const [data, setData] = useState({});
     useEffect(() => {
         fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`
-        ).then((res) => res.json()).then((res) => setData(res[currency]) )
-    } , [currency])
+        ).then((res) => res.json()).then((res) => setData(res[currency]));
+    }, [currency]);
     console.log(data);
-    return data
+    return data;
 }
 
-export default useCurrency
+export default useCurrency;
